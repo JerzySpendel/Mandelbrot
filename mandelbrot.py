996809805a9ecf6d460a5af84eafcf3e4f632179
Jerzy Spendel <jspendel@gmail.com>
2015-02-22 21:14:22 +0100

    After OpenCL calculations only first and second column are transformed

--- mandelbrot.py
+++ mandelbrot.py
@@ -12,16 +12,15 @@ class MandelImage:
     def __init__(self, size=(500, 500)):
         self.size = size
         self.im = Image.new('RGB', self.size)
-        self.area = Area((-2, 2), 4, 4, 500, 500)
+        self.area = Area((-2, 2), 4, 4, size[0], size[1])
 
     def _get_mandel_points(self):
         points = self.area.opencl_mandelbrot()
-        points *= self.size[0]/4
-        points += self.size[0]/2
+        points[:, [0, 1]] *= self.size[0]/4
+        points[:, [0, 1]] += self.size[0]/2
         points = points.astype(np.int32)
         for point in points:
-            # Sadly, but third column is also multiplicated :/
-            if point[2] == int(1*self.size[0]/4 + self.size[1]/2):
+            if point[2] == 1:
                 yield point[0], point[1]
 
     def drawMandel(self):
